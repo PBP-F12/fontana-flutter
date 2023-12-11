fe4c8d0854236c57f9ef9f2b5c8db2119515d935
Arxeno <rmfbkw04@gmail.com>
2023-12-12 00:32:18 +0700

    feat: register as reader integration

--- lib/feature/auth/screens/register_as_reader.dart
+++ lib/feature/auth/screens/register_as_reader.dart
@@ -1,5 +1,6 @@
 import 'package:bookshelve_flutter/feature/auth/screens/register_as_author.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ReaderRegistrationPage extends StatefulWidget {
   const ReaderRegistrationPage({super.key});
@@ -11,6 +12,8 @@ class ReaderRegistrationPage extends StatefulWidget {
 class _ReaderRegistrationPageState extends State<ReaderRegistrationPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmationPasswordController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +35,12 @@ class _ReaderRegistrationPageState extends State<ReaderRegistrationPage> {
               controller: _passwordController,
               obscureText: true,
               decoration: InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _confirmationPasswordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Confirm Password'),
             ),
             const SizedBox(height: 24.0),
             ElevatedButton(
@@ -76,11 +85,27 @@ class _ReaderRegistrationPageState extends State<ReaderRegistrationPage> {
         _passwordController.text.isNotEmpty;
   }
 
-  void _performRegistration(BuildContext context) {
+  void _performRegistration(BuildContext context) async {
     // Replace this with your actual registration logic
     // For simplicity, we'll just print a message
-    print('Registration logic here');
-    Navigator.pop(context);
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+    String confirmationPassword = _confirmationPasswordController.text;
+
+    Map<String, dynamic> request = {
+      "username": username,
+      "password1": password,
+      "password2": confirmationPassword
+    };
+
+    final uri = Uri.parse("http://localhost:8000/auth/api/register/reader");
+    final response = await http.post(uri, body: request);
+
+    if (response.statusCode == 200) {
+      Navigator.pop(context);
+    } else {
+      print('error');
+    }
   }
 
   void _showErrorDialog() {
