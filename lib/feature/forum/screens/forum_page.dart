4b4d3cb14a7d639cc3e0c45e43f04440372c7702
Arxeno <rmfbkw04@gmail.com>
2023-12-17 10:57:38 +0700

    feat: forum page (integrate)

--- lib/feature/forum/screens/forum_page.dart
+++ lib/feature/forum/screens/forum_page.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:bookshelve_flutter/feature/forum/screens/forum_create_form.dart';
 import 'package:bookshelve_flutter/feature/forum/screens/forum_detail.dart';
 import 'package:bookshelve_flutter/feature/forum/widgets/forum_card.dart';
 import 'package:bookshelve_flutter/utils/cookie.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ForumMainPage extends StatefulWidget {
   final CookieRequest request;
@@ -14,12 +17,28 @@ class ForumMainPage extends StatefulWidget {
 }
 
 class _ForumMainPageState extends State<ForumMainPage> {
-  List<ForumItem> forumItems = [];
+  late Future<dynamic> forums;
 
   CookieRequest request = CookieRequest();
 
   _ForumMainPageState(CookieRequest request) {
     this.request = request;
+  }
+
+  Future<dynamic> getForums() async {
+    final responseBody = await request.get('http://localhost:8000/forum/api');
+
+    if (responseBody['status'] == 200) {
+      return responseBody['forums'];
+    } else {
+      throw 'Failed';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    forums = getForums();
   }
 
   @override
@@ -28,26 +47,53 @@ class _ForumMainPageState extends State<ForumMainPage> {
       appBar: AppBar(
         title: Text('Forum Main Page'),
       ),
-      body: ListView.builder(
-        itemCount: forumItems.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(forumItems[index].title),
-              subtitle: Text(forumItems[index].bookTopic),
-              onTap: () {
-                // Navigate to the forum detail page when a card is pressed
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ForumDetailPage(forumItems[index]),
-                  ),
-                );
-              },
-            ),
-          );
-        },
-      ),
+      body: FutureBuilder(
+          future: forums,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return const Center(child: Text('error'));
+              }
+
+              if (!snapshot.hasData) {
+                return const Center(child: Text('no data'));
+              }
+
+              List<dynamic> forums = snapshot.data;
+
+              // return Text('success');
+
+              return ListView.builder(
+                itemCount: forums.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(forums[index]['forumTitle']),
+                      subtitle: Text(forums[index]['book']['title']),
+                      onTap: () {
+                        // Navigate to the forum detail page when a card is pressed
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                ForumDetailPage(forums[index]),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return const Center(
+                child: Text('failed to fetch'),
+              );
+            }
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           // Navigate to the forum creation page and wait for the result
@@ -60,9 +106,7 @@ class _ForumMainPageState extends State<ForumMainPage> {
 
           // If the result is not null (user created a forum), add it to the list
           if (result != null) {
-            setState(() {
-              forumItems.add(result);
-            });
+            setState(() {});
           }
         },
         child: Icon(Icons.add),
