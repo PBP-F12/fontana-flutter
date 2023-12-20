b84614cecb9aad2e00da0d3f4ae6ed3842174635
Arxeno <rmfbkw04@gmail.com>
2023-12-20 07:17:10 +0700

    fix: import error

--- lib/feature/auth/screens/login.dart
+++ lib/feature/auth/screens/login.dart
@@ -1,8 +1,6 @@
 import 'package:bookshelve_flutter/feature/auth/screens/register.dart';
-import 'package:bookshelve_flutter/feature/auth/screens/register_as_reader.dart';
 import 'package:bookshelve_flutter/feature/auth/widgets/custom_text_field.dart';
 import 'package:bookshelve_flutter/feature/home/screens/base.dart';
-import 'package:bookshelve_flutter/feature/home/screens/home.dart';
 import 'package:bookshelve_flutter/feature/onboarding/widgets/custom_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:bookshelve_flutter/utils/cookie.dart';
--- lib/feature/onboarding/screens/welcome_page.dart
+++ lib/feature/onboarding/screens/welcome_page.dart
@@ -1,6 +1,5 @@
 import 'package:bookshelve_flutter/feature/auth/screens/login.dart';
 import 'package:bookshelve_flutter/feature/auth/screens/register.dart';
-import 'package:bookshelve_flutter/feature/auth/screens/register_as_reader.dart';
 import 'package:bookshelve_flutter/feature/onboarding/widgets/custom_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
