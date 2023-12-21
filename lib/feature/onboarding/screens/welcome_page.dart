91daff831451e17c9d1f503722d2bd63cbce8d9e
Arxeno <rmfbkw04@gmail.com>
2023-12-21 14:23:12 +0700

    fix: overflow in welcome page

--- lib/feature/onboarding/screens/welcome_page.dart
+++ lib/feature/onboarding/screens/welcome_page.dart
@@ -12,88 +12,88 @@ class WelcomePage extends StatelessWidget {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                  Color.fromARGB(255, 200, 174, 125),
-                  Color(0xFF765827)
-                ])),
-            child: Column(children: [
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Container(
-                  width: screenWidth,
-                  decoration: const BoxDecoration(
-                      color: Color(0xffc8ae7d),
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Color(0x22765827),
-                            blurRadius: 10,
-                            offset: Offset(5, 5))
-                      ]),
-                  padding: const EdgeInsets.only(top: 50, bottom: 50),
-                  child: Image.asset(
-                    'assets/images/readbook-vector.png',
-                    height: 275,
-                  ),
+        body: SingleChildScrollView(
+      child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                Color.fromARGB(255, 200, 174, 125),
+                Color(0xFF765827)
+              ])),
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                width: screenWidth,
+                decoration: const BoxDecoration(
+                    color: Color(0xffc8ae7d),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color(0x22765827),
+                          blurRadius: 10,
+                          offset: Offset(5, 5))
+                    ]),
+                padding: const EdgeInsets.only(top: 50, bottom: 50),
+                child: Image.asset(
+                  'assets/images/readbook-vector.png',
+                  height: 275,
                 ),
               ),
-              const SizedBox(height: 40),
-              Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Welcome to Fontana.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 36,
-                            fontWeight: FontWeight.bold,
-                            color: const Color(0xFF333333),
-                            fontFamily:
-                                GoogleFonts.dmSerifDisplay().fontFamily),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Explore this limitless world in the palm of your hand!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: const Color(0xFF333333),
-                            fontSize: 18,
-                            fontFamily: GoogleFonts.merriweather().fontFamily),
-                      ),
-                      const SizedBox(height: 40),
-                      CustomTextButton(
-                          buttonText: 'Sign in',
-                          fixedSize:
-                              MaterialStateProperty.all(Size(screenWidth, 60)),
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: ((context) => const LoginPage())));
-                          },
-                          variant: 'primary'),
-                      const SizedBox(height: 10),
-                      CustomTextButton(
-                        buttonText: 'Sign up',
+            ),
+            const SizedBox(height: 40),
+            Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      'Welcome to Fontana.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xFF333333),
+                          fontFamily: GoogleFonts.dmSerifDisplay().fontFamily),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Explore this limitless world in the palm of your hand!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: const Color(0xFF333333),
+                          fontSize: 18,
+                          fontFamily: GoogleFonts.merriweather().fontFamily),
+                    ),
+                    const SizedBox(height: 40),
+                    CustomTextButton(
+                        buttonText: 'Sign in',
                         fixedSize:
                             MaterialStateProperty.all(Size(screenWidth, 60)),
                         onPressed: () {
                           Navigator.pushReplacement(
                               context,
                               MaterialPageRoute(
-                                  builder: ((context) =>
-                                      const RegisterPage())));
+                                  builder: ((context) => const LoginPage())));
                         },
-                        variant: 'secondary',
-                      )
-                    ],
-                  )),
-            ])));
+                        variant: 'primary'),
+                    const SizedBox(height: 10),
+                    CustomTextButton(
+                      buttonText: 'Sign up',
+                      fixedSize:
+                          MaterialStateProperty.all(Size(screenWidth, 60)),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => const RegisterPage())));
+                      },
+                      variant: 'secondary',
+                    )
+                  ],
+                )),
+          ])),
+    ));
   }
 }
