2e8b5dda9de0acb1d5261741f95e6ffdb4ba7a27
Arxeno <rmfbkw04@gmail.com>
2024-01-03 14:28:12 +0700

    add scrollview

--- lib/feature/forum/screens/forum_detail.dart
+++ lib/feature/forum/screens/forum_detail.dart
@@ -58,82 +58,85 @@ class _ForumDetailPageState extends State<ForumDetailPage> {
                     color: FontanaColor.creamy0,
                     fontFamily: GoogleFonts.merriweather().fontFamily)),
             backgroundColor: const Color.fromARGB(255, 132, 112, 73)),
-        body: FutureBuilder(
-            future: forumDetail,
-            builder: ((context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return const Center(child: Text('error'));
+        body: SingleChildScrollView(
+          child: FutureBuilder(
+              future: forumDetail,
+              builder: ((context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return const Center(child: Text('error'));
+                  }
+
+                  if (!snapshot.hasData) {
+                    return const Center(child: Text('no data'));
+                  }
+
+                  // return const Text('data');
+                  dynamic forum = snapshot.data;
+                  List<dynamic> replies = forum['replies'];
+
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Title: ' + forum['forumTitle'],
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text('Book Topic: ' + forum['book']['title']),
+                        const SizedBox(height: 8.0),
+                        Text('Discussion: ' + forum['forumDiscussion']),
+                        const SizedBox(height: 16.0),
+                        const Text(
+                          'Comments:',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 8.0),
+                        replies.isNotEmpty
+                            ? _buildCommentList(replies)
+                            : const Center(child: Text('No Comments.')),
+                        const SizedBox(height: 16.0),
+                        TextField(
+                          controller: _commentController,
+                          decoration:
+                              const InputDecoration(labelText: 'Add a comment'),
+                        ),
+                        const SizedBox(height: 8.0),
+                        CustomTextButton(
+                            buttonText: 'Add Comment',
+                            onPressed: () async {
+                              final requestBody = await request.postJson(
+                                  '${Urls.backendUrl}/forum/api/reply/$forumId',
+                                  jsonEncode(<String, String>{
+                                    'comment': _commentController.text
+                                  }));
+
+                              if (requestBody['status'] == 200) {
+                                setState(() {
+                                  forumDetail = getForumDetail();
+                                  _commentController.clear();
+                                });
+                              }
+                            }),
+                      ],
+                    ),
+                  );
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  return const Center(
+                    child: Text('failed to fetch'),
+                  );
                 }
-
-                if (!snapshot.hasData) {
-                  return const Center(child: Text('no data'));
-                }
-
-                // return const Text('data');
-                dynamic forum = snapshot.data;
-                List<dynamic> replies = forum['replies'];
-
-                return Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Title: ' + forum['forumTitle'],
-                        style: const TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 8.0),
-                      Text('Book Topic: ' + forum['book']['title']),
-                      const SizedBox(height: 8.0),
-                      Text('Discussion: ' + forum['forumDiscussion']),
-                      const SizedBox(height: 16.0),
-                      const Text(
-                        'Comments:',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 8.0),
-                      replies.isNotEmpty
-                          ? _buildCommentList(replies)
-                          : const Center(child: Text('No Comments.')),
-                      const SizedBox(height: 16.0),
-                      TextField(
-                        controller: _commentController,
-                        decoration:
-                            const InputDecoration(labelText: 'Add a comment'),
-                      ),
-                      const SizedBox(height: 8.0),
-                      CustomTextButton(
-                          buttonText: 'Add Comment',
-                          onPressed: () async {
-                            final requestBody = await request.postJson(
-                                '${Urls.backendUrl}/forum/api/reply/$forumId',
-                                jsonEncode(<String, String>{
-                                  'comment': _commentController.text
-                                }));
-
-                            if (requestBody['status'] == 200) {
-                              setState(() {
-                                forumDetail = getForumDetail();
-                                _commentController.clear();
-                              });
-                            }
-                          }),
-                    ],
-                  ),
-                );
-              } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else {
-                return const Center(
-                  child: Text('failed to fetch'),
-                );
-              }
-            })));
+              })),
+        ));
   }
 
   Widget _buildCommentList(List<dynamic> comments) {
